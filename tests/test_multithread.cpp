c6e6f3ad2c351288199d044cf42b65d537ecc24a
uchenily <uchenily@qq.com>
2024-05-30 22:41:48 +0800

    test multithraed

---
+++ tests/test_multithread.cpp
@@ -0,0 +1,66 @@
+#include "asio.hpp"
+#include "debug.hpp"
+
+using namespace std::literals;
+
+class printer {
+public:
+    printer(asio::io_context &io)
+        : strand_{asio::make_strand(io)}
+        , timer1_{io, 1s}
+        , timer2_{io, 1s}
+        , count_{0} {
+        timer1_.async_wait([this](auto) {
+            print1();
+        });
+        timer2_.async_wait([this](auto) {
+            print2();
+        });
+    }
+
+    ~printer() {
+        LOG_INFO("Final count is {}", count_);
+    }
+
+    void print1() {
+        if (count_ < 10) {
+            LOG_INFO("timer1 count={}", count_);
+            count_++;
+
+            timer1_.expires_at(timer1_.expiry() + 1s);
+            timer1_.async_wait([this](auto) {
+                print1();
+            });
+        }
+    }
+
+    void print2() {
+        if (count_ < 10) {
+            LOG_INFO("timer2 count={}", count_);
+            count_++;
+
+            timer2_.expires_at(timer2_.expiry() + 1s);
+            timer2_.async_wait([this](auto) {
+                print2();
+            });
+        }
+    }
+
+private:
+    asio::strand<asio::io_context::executor_type> strand_;
+    asio::steady_timer                            timer1_;
+    asio::steady_timer                            timer2_;
+    int                                           count_;
+};
+
+auto main() -> int {
+    asio::io_context io;
+
+    printer p{io};
+    // 现在有两个线程: 主线程, 和一个额外的线程
+    std::thread t{[&io]() {
+        io.run();
+    }};
+    // 在所有异步操作完成之前, 后台线程 t 不会退出
+    t.join();
+}
