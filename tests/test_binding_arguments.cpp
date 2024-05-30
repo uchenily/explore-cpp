ada5f3293ec4c01529c60049b965cfc98e727236
uchenily <uchenily@qq.com>
2024-05-30 22:01:36 +0800

    test binding arguments

---
+++ tests/test_binding_arguments.cpp
@@ -0,0 +1,27 @@
+#include "asio.hpp"
+#include "debug.hpp"
+
+using namespace std::literals;
+using namespace asio::error;
+
+void print(const asio::error_code &ec, asio::steady_timer *t, int *count) {
+    LOG_INFO("callback called, ec={}", ec.message());
+    if (*count < 5) {
+        LOG_INFO("count={}", *count);
+        (*count)++;
+
+        t->expires_at(t->expiry() + 1s);
+        t->async_wait(std::bind(print, asio::placeholders::error, t, count));
+    }
+}
+
+auto main() -> int {
+    asio::io_context   io;
+    int                count = 0;
+    asio::steady_timer t{io, 1s};
+
+    t.async_wait(std::bind(print, asio::placeholders::error, &t, &count));
+
+    io.run();
+    LOG_INFO("Final count is {}", count);
+}
