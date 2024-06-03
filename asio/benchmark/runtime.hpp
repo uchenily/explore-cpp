7fbd50f0356aa8fe322b81dc44d82ac984781713
uchenily <uchenily@qq.com>
2024-06-03 17:55:18 +0800

    Add runtime.hpp

---
+++ asio/benchmark/runtime.hpp
@@ -0,0 +1,10 @@
+#include "task.hpp"
+
+inline void spawn(Task<> &&task) {
+    auto main_coro = [](Task<> task) -> Task<void> {
+        co_await task;
+    }(std::move(task));
+
+    auto handle = main_coro.take();
+    handle.resume();
+}
