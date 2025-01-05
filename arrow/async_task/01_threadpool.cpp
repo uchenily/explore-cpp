b2e0eaaa7bf1f6dfb2194fd2dacb19faf15089a5
uchenily <uchenily@qq.com>
2025-01-05 22:10:37 +0800

    [arrow]: wait all futures

--- arrow/async_task/01_threadpool.cpp
+++ arrow/async_task/01_threadpool.cpp
@@ -15,15 +15,30 @@ auto RunMain() -> Status {
     ARROW_ASSIGN_OR_RAISE(auto pool, ThreadPool::Make(4));
 
     // 提交一个异步任务到线程池
-    ARROW_ASSIGN_OR_RAISE(auto future, pool->Submit([]() -> arrow::Result<int> {
-        std::this_thread::sleep_for(1s); // 模拟耗时任务
-        return 100;
-    }));
+    ARROW_ASSIGN_OR_RAISE(auto future1,
+                          pool->Submit([]() -> arrow::Result<int> {
+                              std::this_thread::sleep_for(1s); // 模拟耗时任务
+                              return 100;
+                          }));
+    ARROW_ASSIGN_OR_RAISE(auto future2,
+                          pool->Submit([]() -> arrow::Result<int> {
+                              std::this_thread::sleep_for(1s); // 模拟耗时任务
+                              return 200;
+                          }));
+    ARROW_ASSIGN_OR_RAISE(auto future3,
+                          pool->Submit([]() -> arrow::Result<int> {
+                              std::this_thread::sleep_for(1s); // 模拟耗时任务
+                              return 300;
+                          }));
 
     // (主线程)等待 Future 完成
     // future.Wait();
-    ARROW_ASSIGN_OR_RAISE(auto res, future.result());
-    std::cout << "result: " << res << '\n';
+    // ARROW_ASSIGN_OR_RAISE(auto res, future.result());
+    auto futures = arrow::All(std::vector{future1, future2, future3});
+    ARROW_ASSIGN_OR_RAISE(auto results, futures.result());
+    for (auto &res : results) {
+        std::cout << "result: " << res.ValueOrDie() << '\n';
+    }
     return Status::OK();
 }
 
